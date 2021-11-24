f77a642667b6cf1260fe58f531f968f5b6362bfa
potatocandie <maftukhatularifin13@gmail.com>
2021-11-25 05:10:59 +0700

    edit kitty

--- kitty.sh
+++ kitty.sh
@@ -2,16 +2,23 @@
 
 # this will positioning my kitty to my favourite window size and position
 
-loggedInUser="$(whoami)"
-
-if [[ $# -eq 0 ]]; then 
+if [[ $1 -eq 0 ]] && [[ $2 -eq 0 ]]; then 
 open -a "Kitty"
 osascript <<EOS
   tell application "System Events" to tell application process "kitty" to tell window 1 to set {size, position} to {{1257, 673}, {11, 36}}
 EOS
-else 
+fi
+
+if [[ ! $1 -eq 0 ]] && [[ $2 -eq 0 ]]; then 
 open -a "Kitty"
 osascript <<EOS
   tell application "System Events" to tell application process "kitty" to tell window 1 to set {size, position} to {{1257, $1}, {11, 36}}
+EOS
+fi
+
+if [[ ! $1 -eq 0 ]] && [[ ! $2 -eq 0 ]]; then 
+open -a "Kitty"
+osascript <<EOS
+  tell application "System Events" to tell application process "kitty" to tell window 1 to set {size, position} to {{$2, $1}, {11, 36}}
 EOS
 fi
