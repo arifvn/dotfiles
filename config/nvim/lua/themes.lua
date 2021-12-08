9e0d01d2281e67b267b0bbb3a0c81cfbb3241294
potatocandie <maftukhatularifin13@gmail.com>
2021-12-08 21:24:41 +0700

    edit theme

--- config/nvim/lua/themes.lua
+++ config/nvim/lua/themes.lua
@@ -1,17 +1,10 @@
--- THEME 1
--- vim.g.nightflytransparent = 1
--- vim.g.nightflycursorcolor = 0
--- vim.g.nightflynormalfloat = 0
--- vim.g.nightflyUnderlineMatchParen = 1
--- vim.g.nightflyVertSplits = 0
--- vim.cmd [[ hi Visual term='reverse' cterm='reverse' guibg='Grey' ]]
--- vim.cmd 'colorscheme nightfly'
-
--- THEME 2
--- vim.cmd 'colorscheme dracula'
-
--- THEME 3
-vim.cmd 'colorscheme onedark'
+vim.g.nightflytransparent = 1
+vim.g.nightflycursorcolor = 0
+vim.g.nightflynormalfloat = 0
+vim.g.nightflyUnderlineMatchParen = 1
+vim.g.nightflyVertSplits = 0
+vim.cmd [[ hi Visual term='reverse' cterm='reverse' guibg='Grey' ]]
+vim.cmd 'colorscheme nightfly'
 
 -- GLOBAL
 vim.cmd [[ hi Normal guibg=NONE ctermbg=NONE ]]
@@ -21,4 +14,4 @@ vim.cmd [[ hi Folded guibg=none ctermbg=NONE ]]
 vim.cmd [[ hi SpecialKey guibg=none ctermbg=NONE ]]
 vim.cmd [[ hi EndOfBuffer guibg=none ctermbg=NONE ]]
 vim.cmd [[ hi SignColumn guibg=NONE guifg=NONE ctermbg=NONE ctermfg=NONE ]]
-vim.cmd [[ hi VertSplit guifg=#091353 guibg=NONE ctermbg=NONE ctermfg=NONE]]
+vim.cmd [[ hi VertSplit guifg=#001428 guibg=NONE ctermbg=NONE ctermfg=NONE]]
