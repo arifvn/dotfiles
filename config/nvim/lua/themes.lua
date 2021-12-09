c787f0081257e1cb1a7f09326681de11d378568a
potatocandie <maftukhatularifin13@gmail.com>
2021-12-09 09:28:31 +0700

    edit theme

--- config/nvim/lua/themes.lua
+++ config/nvim/lua/themes.lua
@@ -3,7 +3,6 @@ vim.g.nightflycursorcolor = 0
 vim.g.nightflynormalfloat = 0
 vim.g.nightflyUnderlineMatchParen = 1
 vim.g.nightflyVertSplits = 0
-vim.cmd [[ hi Visual term='reverse' cterm='reverse' guibg='Grey' ]]
 vim.cmd 'colorscheme nightfly'
 
 -- GLOBAL
@@ -15,3 +14,4 @@ vim.cmd [[ hi SpecialKey guibg=none ctermbg=NONE ]]
 vim.cmd [[ hi EndOfBuffer guibg=none ctermbg=NONE ]]
 vim.cmd [[ hi SignColumn guibg=NONE guifg=NONE ctermbg=NONE ctermfg=NONE ]]
 vim.cmd [[ hi VertSplit guifg=#001428 guibg=NONE ctermbg=NONE ctermfg=NONE]]
+vim.cmd [[ hi Visual term='reverse' cterm='reverse' guibg='Grey' ]]
