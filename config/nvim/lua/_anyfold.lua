336ea50334fa980a27192fb93841b27085eabce1
potatocandie <maftukhatularifin13@gmail.com>
2022-01-24 17:54:00 +0700

    edit anyfold

--- config/nvim/lua/_anyfold.lua
+++ config/nvim/lua/_anyfold.lua
@@ -1,4 +1,4 @@
 vim.cmd [[
-  autocmd Filetype html AnyFoldActivate " activate for a specific filetype
+  autocmd Filetype lua,markdown,sh,json,javascript,typescript,css,scss,sass,less,html AnyFoldActivate " activate for a specific filetype
 	set foldlevel=99  " open all folds
 ]]
